b76adc2b8400e278851f9bb79e76f8465df16413
user <user@DESKTOP-T5MI7TC>
2023-01-19 13:25:09 +0530

    test commit

---
+++ src/test/java/TextHear/Test2.java
@@ -0,0 +1,20 @@
+package TextHear;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+@Test
+public void create() {
+	String BROWSER = System.getProperty("browser");
+	String URL = System.getProperty("url");
+	String USERNAME = System.getProperty("username");
+	String PASSWORD = System.getProperty("password");
+	System.out.println(BROWSER);
+	System.out.println(URL);
+	System.out.println(USERNAME);
+	System.out.println(PASSWORD);
+	
+	
+	
+}
+}
---
+++ src/test/java/TextHear/Text1.java
@@ -0,0 +1,5 @@
+package TextHear;
+
+public class Text1 {
+
+}
